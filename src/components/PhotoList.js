47a952bb17ed4c44f7e1c939d077ba983865bb5f
Ahmed Roble <airoble2019@gmail.com>
2021-06-18 23:42:08 -0400

    Added stateless and stateful app components

---
+++ src/components/PhotoList.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Photo from "./Photo"
+import NoPhotos from "./NoPhotos"
+
+const PhotoList = (props) => {
+  return (
+    <div className="photo-container">
+      <h2>Results</h2>
+      <PhotoList />
+    </div>
+  )
+}
